87066afb88aa1dd275f318ee6dff6b7bb3f062f9
MrAgrawal1301 <1983mridulagarwal@gmail.com>
2025-06-08 15:22:18 +0530

    renamed find.py and mainfile.py to task1...

--- task1_mainfile.py
+++ task1_mainfile.py
@@ -1,4 +1,4 @@
-from find import delete_branches
+from task1_find_stale_repo import delete_branches
 print("Path of stale branches are:")
 print(*delete_branches , sep="\n")
 if len(delete_branches)==0:
